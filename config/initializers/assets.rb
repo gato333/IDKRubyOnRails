ae9a3316d9e530d6fe5876fb4d396f4314a32c67
Gabriela Voll <gabriela.voll@youvisit.com>
2015-08-23 18:03:06 -0400

    trying to fix heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,16 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( noscript.css )
-
-Rails.application.config.assets.precompile += %w( style.css )
-
-Rails.application.config.assets.precompile += %w( style-desktop.css )
-
-Rails.application.config.assets.precompile += %w( skel-noscript.css )
-
-Rails.application.config.assets.precompile += %w( init.js )
-
-Rails.application.config.assets.precompile += %w( skel.min.js )
-
-Rails.application.config.assets.precompile += %w( jquery.min.js )
+ 
