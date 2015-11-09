c21eef8b1f3fff1d17c2acc31eb607f850566c7b
Gabriela Voll <gabriela.voll@youvisit.com>
2015-11-09 15:18:39 -0500

    trying to fix long ajax call problem with this

---
+++ public/assets/result-9fa2dc43a1ffe552b3d5404b5d76ad34.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".learn_more_button").on("click",function(){$(".learn_more_box").addClass("hidden"),$(this).parent().children(".learn_more_box").removeClass("hidden")})});
