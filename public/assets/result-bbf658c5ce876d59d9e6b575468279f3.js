43d4db1ee231a35786e609f7321ea9c198eb20bf
Gabriela Voll <gabriela.voll@youvisit.com>
2015-12-05 15:50:14 -0500

    rebulding new assets

--- public/assets/result-bbf658c5ce876d59d9e6b575468279f3.js
+++ public/assets/result-bbf658c5ce876d59d9e6b575468279f3.js
@@ -1,11 +1,12 @@
 
-
 $(document).ready( function() {
 	$('.learn_more_button').on("click", function(){
 		if( $(this).parent().children(".learn_more_box").hasClass("hidden") ){
 			$(".learn_more_box").addClass("hidden");
+			$(".learn_more_box").val("expand");
 			$(this).parent().children(".learn_more_box").removeClass("hidden"); 
 			$(this).val("hide");
+			$('body, html').animate({ scrollTop: $($(this).parent()).offset().top });
 		} else {
 			$(this).parent().children(".learn_more_box").addClass("hidden"); 
 			$(this).val("expand");
