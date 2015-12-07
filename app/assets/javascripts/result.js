2cf410a1d9a7e0fc791cad300878228475642f7e
Gabriela Voll <gabriela.voll@youvisit.com>
2015-12-06 21:02:53 -0500

    minor slip up, fixd it tho

--- app/assets/javascripts/result.js
+++ app/assets/javascripts/result.js
@@ -3,7 +3,7 @@ $(document).ready( function() {
 	$('.learn_more_button').on("click", function(){
 		if( $(this).parent().children(".learn_more_box").hasClass("hidden") ){
 			$(".learn_more_box").addClass("hidden");
-			$(".learn_more_box").val("expand");
+			$(".learn_more_button").val("expand");
 			$(this).parent().children(".learn_more_box").removeClass("hidden"); 
 			$(this).val("hide");
 			$('body, html').animate({ scrollTop: $($(this).parent()).offset().top });
