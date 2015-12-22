db15010d78f2b823684bfaf9a4b01848e62935bf
Gabriela Voll <gabriela.voll@youvisit.com>
2015-12-21 19:46:33 -0500

    commenting out parts that should not yet be exposed on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -27,13 +27,13 @@ Rails.application.routes.draw do
   get '/all'  => 'application#all'
 
 
-  get '/index' => 'event_results#index'
+  #get '/index' => 'event_results#index'
 
-  get '/event' => 'event_results#index'
+  #get '/event' => 'event_results#index'
 
   get '/event/new' => 'event_results#new'
 
   get '/event/:id' => 'event_results#show'
 
-  get '/event/:id/edit' => 'event_results#edit'
+  #get '/event/:id/edit' => 'event_results#edit'
 end
