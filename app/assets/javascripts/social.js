09c24f1bed9f608045763dd73d122a29be679716
Gabriela Voll <gabriela.voll@youvisit.com>
2016-01-03 02:32:08 -0500

    pulled in descritpion from the event so it could be sent in the emailer, when i make the handler for it, and got the url with protocol

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -1,17 +1,26 @@
+function openMailer(name, description, url){
+
+}
+
+function closeMailer(){
+
+
+}
 
 $(document).ready( function(){
 
 	$("a.social").on( 'click', function(e){
-		e.preventDefault()
+		e.preventDefault(); 
+
 		var url = "", 
 				id = $(this).parent()[0].id, 
-				currentUrl = encodeURIComponent(window.location.host + "/event/" + id), 
+				currentUrl = window.location.origin + "/event/" + id, 
+				description =  $(this).parent().children("input#description")[0].value, 
 				eventName = $(this).parent().parent().children("h3")[0] || $(this).parent().parent().children(".text").children("h3")[0]; 
 		eventName = eventName.innerHTML; 
-		eventName = eventName.substr(0, eventName.indexOf(' ', 50)); 
+		eventName = eventName.substr(0, eventName.indexOf(' ', 40)); 
 		eventName = encodeURIComponent(eventName); 
 
-		console.log(eventName); console.log( currentUrl); 
 		if( $(this).hasClass("facebook") ){
 			url = "https://www.facebook.com/sharer/sharer.php?u=" + currentUrl;
 		} else if ( $(this).hasClass("twitter") ) {
@@ -19,14 +28,15 @@ $(document).ready( function(){
 		} else if ( $(this).hasClass("google") ) {
 			url = "https://plus.google.com/share?url=" + currentUrl; 
 		} else if ( $(this).hasClass("email") ) {
-			url = "mailto:all@idknyc.com?subject=" + eventName + "%2Fat%2FIDK%2FNYC%2F" + currentUrl; 
+			openMailer( decodeURIComponent(eventName), description, decodeURIComponent(currentUrl) ); 
+			return;
 		} else if ( $(this).hasClass("tumblr") ) {
 			url = "http://www.tumblr.com/share/link?url="  + currentUrl; 
 		} else { 
 			console.log("weirdness, not valid social media");
 			return;
 		}
-		var newwindow = window.open(url, "Sharing IDK NYC Events", 'height=550,width=350');
+		var newwindow = window.open(url, "Sharing IDK NYC Events", 'height=550,width=500');
 		newwindow.focus();
 
 	}); 
