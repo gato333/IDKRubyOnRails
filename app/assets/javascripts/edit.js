9782c662dd583b85066b2c3efb27a4602ed2d0a6
Gabriela Voll <gabriela.voll@youvisit.com>
2016-01-20 23:33:00 -0500

    spacing fix up

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,6 +1,7 @@
 
 $(document).ready( function() {
 	$( "#event_result_startdate, #event_result_enddate" ).datepicker();
+	
 	$("input#event_result_imageurl").on('change input', function(){
 		$("img").attr("src", this.value); 
 	})
