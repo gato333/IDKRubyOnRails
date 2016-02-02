1c987c1c2a24473a6a359d3cc22d9c5d8361d745
Gabriela Voll <gabriela.voll@youvisit.com>
2016-02-01 23:40:20 -0500

    fixing everything up all nice and good

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201064650) do
+ActiveRecord::Schema.define(version: 20160202043200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,12 +35,16 @@ ActiveRecord::Schema.define(version: 20160201064650) do
     t.integer  "creator_id"
   end
 
+  add_index "event_results", ["creator_id", "created_at"], name: "index_event_results_on_creator_id_and_created_at", using: :btree
+
   create_table "user_events", force: true do |t|
     t.integer  "user_id"
     t.integer  "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "user_events", ["user_id", "event_id"], name: "index_user_events_on_user_id_and_event_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
